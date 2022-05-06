449491875287a3583de8f1783ee186d5e5e09957
최지원 <jimmong8702@gmail.com>
2022-05-06 23:14:44 +0900

    jsx, fragment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
+import { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>안녕 리액트!</h1>);
+
+//첫 번째 아규먼트 값을 바탕으로 새로운 html 요소를 만든다.
+//그 값을 두 번째 아규먼트 값에다가 집어넣는 방식으로 동작.
+const root = ReactDOM.createRoot(document.getElementById('root'));  //html에 있는 root 요소 id 값을 가져온다.
+root.render(<p className="hello">안녕 리액트!</p>);
+
+root.render(
+    <form>
+        <label htmlFor='name'>이름</label>
+        <input id='name' type='text' onBlur='' onFocus='' onMouseDown=''/>
+    </form>
+);
+
+const root1 = ReactDOM.createRoot(document.getElementById('root1'));
+
+//Fragment를 생략하고 <> </> 로도 작성가능.
+root1.render(
+    <Fragment>
+        <p>안녕</p>
+        <p>리액트!</p>
+    </Fragment>
+);
