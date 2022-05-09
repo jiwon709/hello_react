1059ce1294cdc6b775edbcaed62095d39bc02d01
최지원 <jimmong8702@gmail.com>
2022-05-09 22:34:27 +0900

    컴포넌트 형식, 지켜야 할 사항

--- src/index.js
+++ src/index.js
@@ -1,41 +1,19 @@
-import { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 
-
-//첫 번째 아규먼트 값을 바탕으로 새로운 html 요소를 만든다.
-//그 값을 두 번째 아규먼트 값에다가 집어넣는 방식으로 동작.
-const root = ReactDOM.createRoot(document.getElementById('root'));  //html에 있는 root 요소 id 값을 가져온다.
-root.render(<p className="hello">안녕 리액트!</p>);
-
-root.render(
-    <form>
-        <label htmlFor='name'>이름</label>
-        <input id='name' type='text' onBlur='' onFocus='' onMouseDown=''/>
-    </form>
-);
-
-const root1 = ReactDOM.createRoot(document.getElementById('root1'));
-
-//Fragment를 생략하고 <> </> 로도 작성가능.
-root1.render(
-    <Fragment>
-        <p>안녕</p>
-        <p>리액트!</p>
-    </Fragment>
-);
-
-const product = 'MacBook';
-const model = 'Air';
-
-function handleClick() {
-    alert('곧 도착합니다!');
+//react 컴포넌트 형식
+//밑에서 쓰이는 Hello 태그는 Hello함수가 return 하는 react element 를 의미
+function Hello() {  //반드시 대문자로 시작
+    return <h1>안녕 리액트</h1>;    //jsx문법으로 쓰인 react Element를 return 해야 한다.
 }
 
-//변수를 집어넣어 줄 때는 {}를 써준다.
-//함수를 실행할 때도 {}를 써준다.
-root1.render(
+const element = (
     <>
-        <h1>나만의 {product.toUpperCase() + model} 주문하기</h1>
-        <button onClick={handleClick}>확인</button>
+        <Hello />
+        <Hello />
+        <Hello />
     </>
 );
+
+//ReactDOM.render(element, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
