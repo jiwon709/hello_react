de92716a893b567b213638f0be69463a712e2880
최지원 <jimmong8702@gmail.com>
2022-05-09 22:21:42 +0900

    jsx 쓰는 방식

--- src/index.js
+++ src/index.js
@@ -23,3 +23,19 @@ root1.render(
         <p>리액트!</p>
     </Fragment>
 );
+
+const product = 'MacBook';
+const model = 'Air';
+
+function handleClick() {
+    alert('곧 도착합니다!');
+}
+
+//변수를 집어넣어 줄 때는 {}를 써준다.
+//함수를 실행할 때도 {}를 써준다.
+root1.render(
+    <>
+        <h1>나만의 {product.toUpperCase() + model} 주문하기</h1>
+        <button onClick={handleClick}>확인</button>
+    </>
+);
