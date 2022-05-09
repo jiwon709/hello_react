9581e2fd5cb8bdeff39d4a64000b8217f5b73585
최지원 <jimmong8702@gmail.com>
2022-05-09 22:42:52 +0900

    Revert "Revert "가위바위보 게임 시작""
    This reverts commit 4cbc3e0d2bb9bfaa676b254bacbf6f8473137199.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import ReactDOM from 'react-dom/client';
-
-//react 컴포넌트 형식
-//밑에서 쓰이는 Hello 태그는 Hello함수가 return 하는 react element 를 의미
-function Hello() {  //반드시 대문자로 시작
-    return <h1>안녕 리액트</h1>;    //jsx문법으로 쓰인 react Element를 return 해야 한다.
-}
-
-const element = (
-    <>
-        <Hello />
-        <Hello />
-        <Hello />
-    </>
-);
+import App from './App';
 
 //ReactDOM.render(element, document.getElementById('root'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(element);
+root.render(<App />);
