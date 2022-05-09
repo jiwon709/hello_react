1edd9a97d5911f2aa8f1da660b6f12c186185759
최지원 <jimmong8702@gmail.com>
2022-05-09 23:04:28 +0900

    props 활용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Dice from "./Dice";
 
+//props : 컴포넌트에 지정한 속성 (properties의 줄임말 / 각각의 속성은 prop 이라고 한다.)
 function App() {
     return (
     <div>
-        <Dice />
+        <Dice color="red" />
     </div>);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-//ReactDOM.render(element, document.getElementById('root'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(<App />);
