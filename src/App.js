c06268a6e1ede741c2ee8a7d1fcad8bd19f9c660
최지원 <jimmong8702@gmail.com>
2022-06-29 23:08:17 +0900

    복습 및 spread 사용 이유

--- src/App.js
+++ src/App.js
@@ -22,6 +22,18 @@ function App() {
         //setGameHistory(gameHistory);    //setter 함수로 새 값이 추가된 gameHistory를 전달한다.
         setGameHistory([...gameHistory, nextNum]);   //gameHistory를 펼치고 뒤에 값을 붙인다.
         //배열이나 객체같은 참조형 state를 사용할 때는 메서드나 할당연산자로 값을 바꾸는게 아니라 반드시 새로운 값을 만들어서 변경해야 한다. (꼭 기억하기 !)
+
+
+        /*   잠깐 설명 타임
+        *    gameHistory : state 는 배열 값 자체를 가지고 있는게 아니라 그 배열의 주솟값을 참조하고 있다.
+        *    때문에 push 메서드로 배열 안에 요소를 변경했다고 하더라도 결과적으로 참조하는 배열의 주솟값은 변경된 것이 아니다.
+        * 
+        *    리액트 입장에서는 gameHistory state가 참조하는 주솟값은 여전히 똑같기 때문에 상태(state)가 바뀌었다고 판단하지 않는 것
+        *    그래서 참조형 state를 활용할 때는 반드시 새로운 참조형 값을 만들어 state를 변경해야 하는데
+        *    가장 간닪나 방법이 Spread 문법(...)을 활용하는 것
+        * 
+        *    참조형 state를 활용할 땐 반드시 새로운 참조형 값을 만들어서 state를 변경해야 한다는 점. 꼭 기억!
+        */
     };
 
     const handleClearClick = () => {
